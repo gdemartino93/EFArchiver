3b27d9ca3a83f03fad39c4e295205596df1a1981
gdemartino93 <g.demartino9393@gmail.com>
2025-04-12 23:56:51 +0200

    [REFACTOR] improve PartitionMetadataHelper comments and overloads

--- EFArchiver/Helpers/PartitionMetadataHelper.cs
+++ EFArchiver/Helpers/PartitionMetadataHelper.cs
@@ -9,14 +9,15 @@ using System.Threading.Tasks;
 namespace EFArchiver.Helpers
 {
     /// <summary>
-    /// retrn metadata from entity classes marked with [PartitionKey]
+    /// utility class to extract metadata from entity market with [PartitionKey]
     /// </summary>
     public static class PartitionMetadataHelper
     {
         /// <summary>
-        /// find the prop marked with [PartitionKey] in the specified entity
+        /// retrieves the prop marked with [PartitionKey] from the specified generic entity type
         /// </summary>
-        /// <returns>The PropertyInfo of the partition key, or null if not found.</returns>
+        /// <typeparam name="T">The entity type to inspect</typeparam>
+        /// <returns>the PropertyInfo of the partition key or null if not found</returns>
         public static PropertyInfo? GetPartitionKeyProperty<T>()
         {
             var type = typeof(T);
@@ -34,12 +35,49 @@ namespace EFArchiver.Helpers
         }
 
         /// <summary>
-        /// retrieves the settings from the [PartitionKey] attribute of the entity type
+        /// retrieves the property marked with [PartitionKey] from the specified CLR type
+        /// This overload is used when iterating through entities dynamically
         /// </summary>
-        /// <returns>return istance of attribute for access at parameters (eg. equalTo, thresholdDays) or NULL if not found</returns>
+        /// <param name="entityType">the CLR type of the entity</param>
+        /// <returns> PropertyInfo of the partition key or null if not found</returns>
+        public static PropertyInfo? GetPartitionKeyProperty(Type entityType)
+        {
+            var props = entityType.GetProperties();
+            foreach (var prop in props)
+            {
+                var attribute = prop.GetCustomAttribute<PartitionKeyAttribute>();
+                if (attribute != null)
+                {
+                    return prop;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// retrieves the [PartitionKey] attribute instance from the specified generic entity type.
+        /// </summary>
+        /// <typeparam name="T">the entity type to inspect</typeparam>
+        /// <returns> PartitionKeyAttribute instance or null if not found.</returns>
         public static PartitionKeyAttribute? GetPartitionKeySettings<T>()
         {
             var prop = GetPartitionKeyProperty<T>();
+            if (prop == null)
+            {
+                return null;
+            }
+            return prop.GetCustomAttribute<PartitionKeyAttribute>();
+        }
+
+        /// <summary>
+        /// retrieves the [PartitionKey] attribute instance from the specified CLR type
+        /// this overload is used when iterating through entities dynamically
+        /// </summary>
+        /// <param name="entityType">the CLR type of the entity</param>
+        /// <returns>PartitionKeyAttribute instance or null if not found</returns>
+        public static PartitionKeyAttribute? GetPartitionKeySettings(Type entityType)
+        {
+            var prop = GetPartitionKeyProperty(entityType);
             if (prop == null)
             {
                 return null;
