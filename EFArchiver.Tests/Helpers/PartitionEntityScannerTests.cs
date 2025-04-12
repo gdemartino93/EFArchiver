8d784b1341988cae172fd49ed40f9f7f4d5962af
gdemartino93 <g.demartino9393@gmail.com>
2025-04-12 23:30:15 +0200

    [TEST] add test for GetPartitionedEntities and PartitionedEntityAttribute

---
+++ EFArchiver.Tests/Helpers/PartitionEntityScannerTests.cs
@@ -0,0 +1,57 @@
+﻿using EFArchiver.Attributes;
+using EFArchiver.Helpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFArchiver.Tests.Helpers
+{
+    public class PartitionEntityScannerTests
+    {
+        private class MyDbContext : DbContext
+        {
+            public DbSet<Person> People => Set<Person>();
+            public DbSet<Animal> Animals => Set<Animal>();
+            public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Person>().ToTable("People");
+                modelBuilder.Entity<Animal>().ToTable("Animals");
+            }
+
+        }
+
+        [PartitionedEntity]
+        private class Person
+        {
+            public Guid Id { get; set; }
+
+            [PartitionKey(ThresholdDays = 30)]
+            public DateTime CreatedAt { get; set; }
+        }
+
+        private class Animal
+        {
+            public Guid Id { get; set; }
+            public string? Name { get; set; }
+        }
+
+        [Fact]
+        public void GetPartitionedEntities_ShouldReturnOnly_Persons()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseInMemoryDatabase("PartitionedDb").Options;
+
+            using var context = new MyDbContext(options);
+
+            //Act
+            var partitionedEntities = PartitionEntityScanner.GetPartitionedEntities(context);
+
+            //Assert
+            var types = partitionedEntities.Select(e => e.ClrType).ToList();
+            Assert.Single(types);
+            Assert.Contains(typeof(Person), types);
+            Assert.DoesNotContain(typeof(Animal), types);
+        }
+
+    }
+}
