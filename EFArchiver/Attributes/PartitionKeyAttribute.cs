658ae3f490e904c8a041882c08e3bf97e4366c18
gdemartino93 <g.demartino9393@gmail.com>
2025-04-12 22:24:26 +0200

    created property decorator PartitionKeyAttribute

---
+++ EFArchiver/Attributes/PartitionKeyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFArchiver.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PartitionKeyAttribute : Attribute
+    {
+        // if the property is a DateTime define the threshoold (eg. older then 365 days)
+        public int? ThresholdDays { get; set; }
+        // if the property is int, bool, enum use this to filter on a specific value
+        public object? EqualTo { get; set; }
+        public PartitionKeyAttribute()
+        {
+            
+        }
+    }
+}
