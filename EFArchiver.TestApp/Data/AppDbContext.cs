8da0346790f3158bbe8323f34ab522ef893f383f
gdemartino93 <g.demartino9393@gmail.com>
2025-04-12 15:33:09 +0200

    [TOFIX] error to get column names

---
+++ EFArchiver.TestApp/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EFArchiver.TestApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFArchiver.TestApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People => Set<Person>();
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Person>().ToTable("People");
+        }
+    }
+}
--- EFArchiver.TestApp/Program.cs
+++ EFArchiver.TestApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFArchiver;
+using EFArchiver.TestApp.Data;
+using EFArchiver.TestApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+var connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=EFArchiverTestDb;Trusted_Connection=True;";
+var options = new DbContextOptionsBuilder<AppDbContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new AppDbContext(options);
+
+context.Database.ExecuteSqlRaw("""
+    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES 
+                   WHERE TABLE_NAME = 'People_Storage' AND TABLE_SCHEMA = 'dbo')
+    BEGIN
+        CREATE TABLE [People_Storage] (
+            [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+            [Name] NVARCHAR(MAX) NOT NULL,
+            [LastName] NVARCHAR(MAX) NOT NULL,
+            [Age] INT NOT NULL,
+            [CreatedAt] DATETIME2 NOT NULL
+        );
+    END
+""");
+
+var archiver = new EntityArchiver<Person>(context);
+await archiver.ArchiveAsync(p => p.CreatedAt < new DateTime(2025, 1, 1), "Storage");
