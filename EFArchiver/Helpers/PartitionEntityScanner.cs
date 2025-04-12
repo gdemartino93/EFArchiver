45596c2c709fd067d24dec56c367798fc25555cb
gdemartino93 <g.demartino9393@gmail.com>
2025-04-12 23:29:32 +0200

    created PartitionedEntityAttribute with decorator target class and helper methos do retrevies the entities marked with decorator

---
+++ EFArchiver/Helpers/PartitionEntityScanner.cs
@@ -0,0 +1,24 @@
+﻿using EFArchiver.Attributes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFArchiver.Helpers
+{
+    public class PartitionEntityScanner
+    {
+        /// <summary>
+        /// return all the entities in DbContext decorated with [PartitionedEntity]
+        /// </summary>
+        public static IEnumerable<IEntityType> GetPartitionedEntities(DbContext context)
+        {
+            return context.Model
+                .GetEntityTypes() // retrieve all classes that have a DbSet
+                .Where(e => e.ClrType.GetCustomAttributes(typeof(PartitionedEntityAttribute), inherit: false).Any()); // see if the original class has decorator  [PartitionedEntity]
+        }
+    }
+}
