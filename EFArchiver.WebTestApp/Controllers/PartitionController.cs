adfa4b5e6ec257b7db67e2329680ce5c53d478c6
gdemartino93 <g.demartino9393@gmail.com>
2025-04-13 21:58:50 +0200

    [TEST][WEBAPI] added controller to test archiver manager in web api project

---
+++ EFArchiver.WebTestApp/Controllers/PartitionController.cs
@@ -0,0 +1,25 @@
+﻿using EFArchiver.WebTestApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFArchiver.WebTestApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PartitionController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PartitionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Partition()
+        {
+            var manager = new EFArchiverManager(_context);
+            await manager.PartitionAllAsync("Storage");
+            return Ok("Archiviazione completata");
+        }
+    }
+}
--- EFArchiver.WebTestApp/Models/Person.cs
+++ EFArchiver.WebTestApp/Models/Person.cs
@@ -1,10 +1,14 @@
-﻿namespace EFArchiver.WebTestApp.Models
+﻿using EFArchiver.Attributes;
+
+namespace EFArchiver.WebTestApp.Models
 {
+    [PartitionedEntity]
     public class Person
     {
         public Guid Id { get; set; }
         public required string Name { get; set; }
         public required string LastName { get; set; }
+        [PartitionKey(ThresholdDays = 90)]
         public DateTime CreatedAt { get; set; }
     }
 }
